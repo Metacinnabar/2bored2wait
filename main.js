133a9f8ab33e96c0ba8fa142db4e020766d0a4d6
Charlie <45357714+GoodPro712@users.noreply.github.com>
2021-07-26 18:25:01 +1000

    Email leak gone
    And removed update doing footer

--- main.js
+++ main.js
@@ -252,7 +252,7 @@ function join() {
 						server.motd = `Place in queue: ${webserver.queuePlace} ETA: ${webserver.ETA}`; // set the MOTD because why not
 						webserver.ETA = Math.floor(ETAmin / 60) + "h " + Math.floor(ETAmin % 60) + "m";
 						if (config.get("userStatus")) { //set the Discord Activity
-							logActivity("P: " + positioninqueue + " E: " + webserver.ETA + " - " + options.username);
+							logActivity("P: " + positioninqueue + " E: " + webserver.ETA + " - " + config.get("minecraftserver.username"));
 						} else {
 							logActivity("P: " + positioninqueue + " E: " + webserver.ETA);
 						}
@@ -424,11 +424,7 @@ function userInput(cmd, DiscordOrigin, discordMsg) {
 									value: `Estimated time until login: **${webserver.ETA}**`
 								}
 							],
-							timestamp: new Date(),
-							footer: {
-								icon_url: dc.user.avatarURL,
-								text: "Author: Surprisejedi"
-							}
+							timestamp: new Date()
 						}
 					});
 					else console.log("Position: " + webserver.queuePlace + "  Estimated time until login: " + webserver.ETA);
